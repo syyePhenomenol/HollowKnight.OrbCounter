af00df068805d4e5d1d0d763930754230e64a2f9
syyePhenomenol <yye3028@gmail.com>
2022-04-01 21:16:16 +1100

    Add ModMenu toggle

--- OrbCounter/OrbCounter.cs
+++ OrbCounter/OrbCounter.cs
@@ -1,32 +1,52 @@
 ﻿using HutongGames.PlayMaker;
 using Modding;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using UnityEngine;
 using Vasi;
 
 namespace HollowKnight.OrbCounter
 {
-    public class OrbCounter : Mod
-    {
-        public static OrbCounter Instance;
+    public class OrbCounter : Mod, IMenuMod, ITogglableMod
+    { 
+        public override string GetVersion() => "1.0.1";
 
-        public override string GetVersion() => "1.0.0";
+        public bool ToggleButtonInsideMenu => true;
 
-        private static FieldInfo spawnedOrbs;
-
-        private static FieldInfo pickedUp;
+        private static readonly FieldInfo spawnedOrbs = typeof(DreamPlant).GetField("spawnedOrbs", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly FieldInfo pickedUp = typeof(DreamPlantOrb).GetField("pickedUp", BindingFlags.Instance | BindingFlags.NonPublic);
 
         private static bool orbPickedUp = false;
 
+        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+        {
+            IMenuMod.MenuEntry e = toggleButtonEntry.Value;
+
+            return new() { new(e.Name, e.Values, "", e.Saver, e.Loader) };
+        }
+
         public override void Initialize()
         {
-            Instance = this;
-
-            spawnedOrbs = typeof(DreamPlant).GetField("spawnedOrbs", BindingFlags.Instance | BindingFlags.NonPublic);
-            pickedUp = typeof(DreamPlantOrb).GetField("pickedUp", BindingFlags.Instance | BindingFlags.NonPublic);
+            // If mod is enabled during the game for the first time
+            if (GameManager.instance.IsGameplayScene())
+            {
+                PlayMakerFSM counterFSM = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Dream Nail").FirstOrDefault(obj => obj.LocateMyFSM("Control") != null).LocateMyFSM("Control");
+                
+                if (counterFSM != null && counterFSM.GetState("Update").Actions.Count() == 3)
+                {
+                    counterFSM.GetState("Update").InsertAction(2, new AddOrbCounter(counterFSM));
+                }
+            }
 
             On.DreamPlantOrb.OnTriggerEnter2D += DreamPlantOrb_OnTriggerEnter2D;
             On.PlayMakerFSM.OnEnable += PlayMakerFSM_OnEnable;
+        }
+
+        public void Unload()
+        {
+            On.DreamPlantOrb.OnTriggerEnter2D -= DreamPlantOrb_OnTriggerEnter2D;
+            On.PlayMakerFSM.OnEnable -= PlayMakerFSM_OnEnable;
         }
 
         private void DreamPlantOrb_OnTriggerEnter2D(On.DreamPlantOrb.orig_OnTriggerEnter2D orig, DreamPlantOrb self, UnityEngine.Collider2D collision)
@@ -62,14 +82,12 @@ namespace HollowKnight.OrbCounter
             {
                 FsmString text = _self.FsmVariables.StringVariables.FirstOrDefault(x => x.Name == "Orbs String");
 
-                DreamPlantOrb[] orbs = UnityEngine.Object.FindObjectsOfType<DreamPlantOrb>();
+                DreamPlantOrb[] orbs = Object.FindObjectsOfType<DreamPlantOrb>();
 
-                DreamPlant plant = UnityEngine.Object.FindObjectOfType<DreamPlant>();
+                DreamPlant plant = Object.FindObjectOfType<DreamPlant>();
 
                 if (orbPickedUp && orbs.Count() > 0 && plant != null)
                 {
-                    orbPickedUp = false;
-
                     int remainingOrbs = (int) spawnedOrbs.GetValue(plant) - 1;
 
                     if (remainingOrbs >= 0)
@@ -77,6 +95,8 @@ namespace HollowKnight.OrbCounter
                         text.Value = text + " (" + (orbs.Count() - remainingOrbs) + "/" + orbs.Count() + ")";
                     }
                 }
+
+                orbPickedUp = false;
 
                 Finish();
             }
